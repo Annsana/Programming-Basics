e6e6d9a5ab0642a24d5820cfbef17d737d48f0b5
Annsana <46561381+Annsana@users.noreply.github.com>
2020-03-26 19:36:52 +0530

    Create area.py

---
+++ area.py
@@ -0,0 +1,7 @@
+#To find area of a rectangle using python
+length=int(input("Enter the length"))
+breadth=int(input("Enter the breadth"))
+area=length*breadth
+print("The length= ",length)
+print("The breadth= ",breadth)
+print("The area = ",area)
